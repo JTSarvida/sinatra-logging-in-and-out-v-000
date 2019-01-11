b876816f26f0debf29aedef19f983896fc58f95e
Joshua Tan <jtsarvida@gmail.com>
2019-01-11 02:41:48 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,5 +1,6 @@
 class Helpers
   
+<<<<<<< HEAD
   def self.current_user(session)
     @user = User.find_by_id(session[:user_id])
     @user
@@ -7,6 +8,12 @@ class Helpers
   
   def self.is_logged_in?(session)
     !!session[:user_id]
+=======
+  def current_user
+  end
+  
+  def is_logged_in?
+>>>>>>> dda39ece39749403e224534732f49e915ffe7b39
   end
   
 end
